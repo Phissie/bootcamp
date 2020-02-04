2f724492127a5be1989514ba67393134a9669d0e
Olufisayo Fagade <fis@ThirdFloorCafe-AP3.localdomain>
2020-02-04 15:22:50 +0000

    picture changed to professional

--- src/App.js
+++ src/App.js
@@ -7,18 +7,25 @@ import react from "./React.png";
 import javascript from "./javascript.svg";
 import html from "./html.svg";
 import "./App.css";
-import bird from "./ezgif.com-resize.gif";
-import portfoilio from "./portfoilio.jpg";
+import pic from "./professionalpicture.jpeg";
 
 function App() {
   return (
     <div>
       <div className="App">
         <div className="heading">
-          <h2>Hello World! I'm a Full-Stack Developer based in London</h2>
+          <p>Hello World! I'm a Full-Stack Developer based in London</p>
         </div>
-        <img src={portfoilio} className="background-image"></img>
-        <img src={bird} alt="bird" align="middle" className="bird" />
+
+        <img src={pic} className="background-image"></img>
+
+        <div className="pictureDescription">
+          Fisayo Fagade
+          <p>
+            {" "}
+            <li>Full Stack Developer</li>
+          </p>
+        </div>
         <div className="overlay"></div>
       </div>
       <div className="logo">
@@ -61,22 +68,31 @@ function App() {
         <div className="text"></div>
       </div>
       <p className="About-Me">
-        <h2> I’m a Full-Stack Developer based in West London. I am passionate about
-        functional programming, learning new languages and improving my
-        proficiency. My passion for technology led me to launch my tech career
-        in 2017 working very long hours for a start-up in Stratford-upon-Avon,
-        UK in a Business Development role. Daily, I convinced clients that our
-        software will solve issues they had often given up on. I decided to
-        learn to code after my long suffering developer colleagues got fed up
-        with me asking them non stop questions about how our software had all
-        it’s functionality. I started learning to Ruby in my free time in Winter
-        2018 after completing a five week course with Google Digital garage that
-        introduced me to Javascript and CSS. In January 2020, I became a full
-        time developer learning Full Stack web development, ReatcJS, Javascript
-        and CSS. When I am not working on my personal projects, I can be found
-        around London attending tech Meetup groups and conferences; Ruby
-        Hacknight London, ReactJs Girls, CodeBar London, IBM Code London. Code
-        Mesh</h2>
+        <h2>
+          {" "}
+          <p>ABOUT ME</p>
+          <p>
+            I’m a Full-Stack Developer based in West London. I am passionate
+            about functional programming, learning new languages and improving
+            my proficiency.
+          </p>
+          I’m a Full-Stack Developer based in West London. I am passionate about
+          functional programming, learning new languages and improving my
+          proficiency. My passion for technology led me to launch my tech career
+          in 2017 working very long hours for a start-up in Stratford-upon-Avon,
+          UK in a Business Development role. Daily, I convinced clients that our
+          software will solve issues they had often given up on. I decided to
+          learn to code after my long suffering developer colleagues got fed up
+          with me asking them non stop questions about how our software had all
+          it’s functionality. I started learning to Ruby in my free time in
+          Winter 2018 after completing a five week course with Google Digital
+          garage that introduced me to Javascript and CSS. In January 2020, I
+          became a full time developer learning Full Stack web development,
+          ReatcJS, Javascript and CSS. When I am not working on my personal
+          projects, I can be found around London attending tech Meetup groups
+          and conferences; Ruby Hacknight London, ReactJs Girls, CodeBar London,
+          IBM Code London. Code Mesh
+        </h2>
       </p>
     </div>
   );
